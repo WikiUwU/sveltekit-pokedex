39acd1b297959664ebb2e5fe6346456755682a8c
Wiktor <wiktor.jaworowski25@gmail.com>
2023-07-17 18:09:57 +0200

    simple display of pokemon generations as a test for trying out Typescript

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { generations } from './generations'
+
+    console.log(generations)
+</script>
+
+{#each generations as generation (generation.id)}
+    <div>
+        <h1>{generation.name}</h1>
+        <p>Main region: {generation.main_region}</p>
+        <p>Games: {generation.games.join(', ')}</p>
+
+    </div>
+{/each}
+
+<style>
+</style>
